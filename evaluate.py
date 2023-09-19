f1d128ed59456c45e5b2bed01fd69fc3a2e20e53
lakpa-tamang9 <ld.tamang25@gmail.com>
2023-09-19 10:11:07 +1000

    feat: Run evaluate for known and unknown

--- evaluate.py
+++ evaluate.py
@@ -3,6 +3,8 @@ import json
 import numpy as np
 import torch
 
+torch.cuda.empty_cache()
+
 from datasets.dataloaders import get_eval_loaders
 from models.vgg import VGG
 from utils import *
@@ -12,53 +14,10 @@ num_trials = 1
 dataset_name = "CIFAR10"
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(device)
 
-
-def predict_scores(
-    net,
-    mapping,
-    dataloader,
-    unknown=False,
-    use_mls=False,
-    use_stmls=False,
-):
-    """Tests data and returns outputs and their ground truth labels.
-    unknown : True if an unknown dataset
-    """
-    X = []
-    y = []
-
-    for i, data in enumerate(dataloader):
-        images, labels = data
-        images = images.to(device)
-
-        if unknown:
-            targets = labels
-        else:
-            targets = torch.Tensor([mapping[x] for x in labels]).long().to(device)
-
-        outputs = net(images)
-        logits = outputs[0]
-        _, predicted = torch.max(logits, 1)
-        mask = predicted == targets
-        logits = logits[mask]
-        targets = targets[mask]
-
-        if use_mls:
-            scores = get_max_logits(logits=logits)
-        elif use_stmls:
-            scores = get_standardized_max_logits(logits=logits)
-
-        scores = get_softmax_prob(logits=logits)
-
-        X += scores.cpu().detach().tolist()
-        y += targets.cpu().tolist()
-
-    X = np.asarray(X)
-    y = np.asarray(y)
-
-    return X, y
-
+all_accuracy = []
+all_auroc = []
 
 for trial in range(start_trial, start_trial + num_trials):
     with open("./datasets/config.json") as f:
@@ -68,7 +27,10 @@ for trial in range(start_trial, start_trial + num_trials):
         dataset_name, trial, config
     )
     net = VGG("VGG19", n_classes=4)
-    checkpoint = torch.load(f"checkpoints/{dataset_name}/{trial}_ckpt.pth")
+    checkpoint = torch.load(
+        f"checkpoint/{dataset_name}/{trial}_ckpt.pth", map_location=torch.device(device)
+    )
+    # map_location = torch.device("cpu")
 
     net = net.to(device)
     print("test")
@@ -79,12 +41,46 @@ for trial in range(start_trial, start_trial + num_trials):
     net.load_state_dict(pretrained_dict)
     net.eval()
 
-    known_pred_scores, known_pred_labels = predict_scores(net, mapping, known_loader)
-    print(f"Evaluating closed set accuracy for trial {trial}")
-    accuracy = get_accuracy(known_pred_scores, known_pred_labels)
+    X = []
+    y = []
 
-    unknown_pred_scores, unknown_pred_labels = predict_scores(
-        net, mapping, unknown_loader
-    )
-    print(f"Evaluating open set AUROC for trial {trial}")
-    auroc = get_auroc(known_pred_scores, unknown_pred_scores)
+    softmax = torch.nn.Softmax(dim=1)
+    with torch.no_grad():
+        for i, data in enumerate(known_loader):
+            images, labels = data
+            targets = torch.Tensor([mapping[x] for x in labels]).long().to(device)
+
+            images = images.to(device)
+            logits = net(images)
+            scores = softmax(logits)
+
+            X += scores
+            y += targets
+
+        X = np.asarray(X)
+        y = np.asarray(y)
+
+        accuracy = get_accuracy(X, y)
+        all_accuracy += [accuracy]
+
+    Xu = []
+    with torch.no_grad():
+        for i, data in enumerate(unknown_loader):
+            images, labels = data
+
+            images = images.to(device)
+            logits = net(images)
+            scores = softmax(logits)
+            Xu += scores
+        Xu = np.asarray(Xu)
+
+        auroc = get_auroc(X, Xu)
+        all_auroc += [auroc]
+
+mean_acc = np.mean(all_accuracy)
+mean_auroc = np.mean(all_auroc)
+
+print("Raw Top-1 Accuracy: {}".format(all_accuracy))
+print("Raw AUROC: {}".format(all_auroc))
+print("Average Top-1 Accuracy: {}".format(mean_acc))
+print("Average AUROC: {}".format(mean_auroc))
